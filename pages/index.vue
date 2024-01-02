aad18be5024206f0392c86f97a3a3ca8037e8797
JeanCRuiz <139879016+JeanCRuiz@users.noreply.github.com>
2024-01-02 10:57:01 -0400

    new feature

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container fluid class="bg-teal grid-container">
     <h1>Version actual "{{ version }}"</h1>
-    <span>by: Jean Ruiz Veriguete</span>
+    <span>by: Jean Ruiz 👨‍💻</span>
   </v-container>
 </template>
 <script lang="ts">
