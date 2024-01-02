2d1089038b19ab92418c4ef45e34c9774a5c85ee
JeanCRuiz <139879016+JeanCRuiz@users.noreply.github.com>
2024-01-02 14:26:43 -0400

    cambios

--- scripts/firebase/firebase-admin.js
+++ scripts/firebase/firebase-admin.js
@@ -1,6 +1,7 @@
 import { initializeApp, cert } from 'firebase-admin/app'
 import { getFirestore } from 'firebase-admin/firestore'
-import jsonKey from 'file:///C:/Users/JeanCarlosRuizVerigu/Desktop/vue-lugotech/increment-version-nuxt/scripts/firebase-key.json' assert {type: 'json'}
+// import jsonKey from 'file:///C:/Users/JeanCarlosRuizVerigu/Desktop/vue-lugotech/increment-version-nuxt/scripts/firebase-key.json' assert {type: 'json'}
+import jsonKey from '../firebase-key.json' assert {type: 'json'}
 
 initializeApp({
   credential: cert(jsonKey)
